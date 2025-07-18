8e85d007dfe41967d3e2779ea850876299a179fc
afridho <afridho@dxtr.asia>
2025-07-18 21:17:22 +0700

    feat(vite): update latest version

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
+import netlify from "@netlify/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "/frontend-mentor-multi-step-form/",
-    plugins: [react()],
+    plugins: [react(), netlify()],
     resolve: {
         alias: {
             "~": path.resolve(__dirname, "./src"),
